182d99e89162ff2696f47a9c1113e20d7f339947
Carlos Anderson <carlosja@med.umich.edu>
2019-06-16 16:41:15 -0400

    Add initial project files

---
+++ ExportPatientData/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using VMS.TPS.Common.Model.API;
+using YamlDotNet.Serialization;
+
+namespace ExportPatientData
+{
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.Error.WriteLine("Missing parameters: patient-id");
+            return;
+        }
+
+        using (var app = Application.CreateApplication(null, null))
+        {
+            var patientId = args[0];
+            var patient = app.OpenPatientById(patientId);
+
+            var serializer = new SerializerBuilder()
+                .EmitDefaults()
+                .WithAttributeOverride<Structure>(e => e.SegmentVolume, new YamlIgnoreAttribute())
+                .WithAttributeOverride<Structure>(e => e.MeshGeometry, new YamlIgnoreAttribute())
+                .WithAttributeOverride<Isodose>(e => e.MeshGeometry, new YamlIgnoreAttribute())
+                .WithAttributeOverride<ControlPoint>(e => e.LeafPositions, new YamlIgnoreAttribute())
+                .Build();
+
+            var patientString = serializer.Serialize(patient);
+            Console.WriteLine(patientString);
+        }
+    }
+}
+}
